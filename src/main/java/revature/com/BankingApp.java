524df3b26b4ca2c6b4d019746eaf5fd61311e2ad
ali alrubaye <alialrubaye1990@hotmail.com>
2022-06-23 16:02:57 -0400

    preparing the main console

--- src/main/java/revature/com/BankingApp.java
+++ src/main/java/revature/com/BankingApp.java
@@ -1,5 +1,6 @@
 package revature.com;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import revature.com.models.Role;
@@ -12,17 +13,71 @@ public class BankingApp {
 	private static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		
+
 		run();
-		
+
 	}
-	
 
 	public static void run() {
+
+		boolean running = true;   //Keeping the main loop to run the program until user quits
 		
-		     // registering template
-			AccountService as = new AccountService();
-			as.viewAllAccounts();
+		while(running) {
+			
+			
+			try {
+				
+				System.out.println("Welcome to the Bank \n"
+						+ "Please press 1 to register if you are a new customer \n"
+						+ "Please press 2 to log-in if you are a current customer \n"
+						+ "Please press 3 to exit the Bank");
+				int entry = scan.nextInt();
+				
+				if(entry == 2) {
+					
+					//Logging-in for an existing user
+					
+				}else if (entry == 1) {
+					
+					//Registration for a new customer
+					
+					
+				} else if(entry == 3) {
+					
+					running = false;   //To quit the program
+					System.out.println("Thank you for using the Bank");
+				} else {
+					
+					System.out.println("invalid input. Please try again");
+				}
+			} catch (InputMismatchException e) {
+				
+				System.out.println("invalid input. Please try again");
+			} finally {
+				
+				scan.nextLine(); 
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		
+//		 registering template
+//		AccountService as = new AccountService();
+//		as.viewAllAccounts();
 //		System.out.println("Enter username");
 //		String username = scan.next();
 //		
@@ -32,8 +87,6 @@ public class BankingApp {
 //		User u = new User(username, password, Role.Customer, null);
 //		UserService us = new UserService();
 //		us.register(u);
-		
-		
-	}
 
+	}
 }
