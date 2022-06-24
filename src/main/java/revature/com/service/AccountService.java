4cae87416deec940ffe85131f2ef1fb49868990b
ali alrubaye <alialrubaye1990@hotmail.com>
2022-06-24 18:36:16 -0400

    implemented withdraw method

--- src/main/java/revature/com/service/AccountService.java
+++ src/main/java/revature/com/service/AccountService.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import revature.com.dao.AccountDao;
 import revature.com.dao.AccountDaoInterface;
+import revature.com.exceptions.InsufficientBalanceException;
+import revature.com.exceptions.InvalidAmountOfMoneyException;
 import revature.com.exceptions.NoAccountsExistException;
 import revature.com.exceptions.UsernameNotFoundException;
 import revature.com.models.Account;
@@ -31,6 +33,29 @@ public class AccountService {
 		for(Account a: accList ) {
 			System.out.println(a);
 		}
+		
+	}
+	
+	//Withdraws amount from account with the id = accId
+	public double withdraw(double amount, int accId) {
+		
+		Account a = adao.findById(accId);
+		if(a==null) {
+			throw new NoAccountsExistException("No such accounts with this id = "+accId);
+		}
+		if(amount <=0) {
+			throw new InvalidAmountOfMoneyException("Amount to withdraw must be greater than 0 $");
+		}
+		if(amount>a.getBalance()) {
+			throw new InsufficientBalanceException("Sorry, insufficient funds in your account");
+		}
+		
+		//If all of the above pass, then balance will be updated.
+		double newBalance = a.getBalance()-amount;
+		
+		adao.updateBalanceById(newBalance, accId);
+		
+		return newBalance;
 		
 	}
 }
